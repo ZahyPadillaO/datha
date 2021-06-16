3dff8ee271a540e488f876f248b7731bb3faea45
Danii Daniel <darwindaniel.dl@gmail.com>
2021-06-16 11:39:56 -0500

    change name class to Operaciones and logic

--- src/suma.ts
+++ src/suma.ts
@@ -1,29 +1,13 @@
-
-export class Suma{
-
+export class Operaciones{
     public a: number;
     public b: number;
-    private _respuesta: number;
-
 
     constructor(a: number, b: number) {
         this.a = a;
         this.b = b;
-        this._respuesta = b + a;
     }
 
-
-    get respuesta(): number {
-        return this._respuesta;
-    }
-
-    set respuesta(value: number) {
-        this._respuesta = value;
+    public suma(a: number, b: number): number {
+        return a + b;
     }
 }
-
-/*
-function suma(a, b){
-    return parseInt(a) + parseInt(b);
-}
-module.exports= suma;*/
